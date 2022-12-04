2e4e7c187dd6bddd62d0dd34aa8c1bdee4ee99fd
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-12-04 14:56:43 +0100

    day 4

---
+++ day4.jl
@@ -0,0 +1,10 @@
+lines = [split.(split(l, ","), "-") for l in readlines(open("data/day4.txt"))]
+start_end = [map(x -> parse.(Int64, x), z) for z in lines]
+container(b) = (b[1] <= b[3] && b[2] >= b[4]) || (b[3] <= b[1] && b[4] >= b[2])
+flat = [container(vcat(a...)) for a in start_end]
+sum(flat)
+
+
+overlap(a) = length(intersect(Set(range(a[1], a[2])), Set(range(a[3], a[4])))) > 0
+flat = [overlap(vcat(a...)) for a in start_end]
+sum(flat)
