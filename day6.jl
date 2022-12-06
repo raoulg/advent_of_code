21d23efd526dc550b422d6813a2d4238422ffc7d
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-12-06 11:39:11 +0100

    removing redundant 'open'

--- day6.jl
+++ day6.jl
@@ -1,4 +1,4 @@
 filename = "data/6.txt"
-signal(n) = [findfirst([(allunique(l[i:i+n-1])) for i in 1:length(l)-n-1]) for l in readlines(open(filename))] .+ (n-1)
+signal(n) = [findfirst([(allunique(l[i:i+n-1])) for i ∈ 1:length(l)-n-1]) for l ∈ readlines(filename)] .+ (n-1)
 signal(4)
 signal(14)
