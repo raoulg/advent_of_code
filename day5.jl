8db9306c90a473a1605398aa907fc4c29f73255c
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-12-06 11:22:40 +0100

    simplified day 5

--- day5.jl
+++ day5.jl
@@ -1,33 +1,19 @@
-function split_input(filename)
-    lines = readlines(open(filename))
-    idx = findfirst(isempty, lines)
-    lines[1:idx-2], lines[idx+1:end]
-end
-
-function parse_schema(schema)
-    levels = []
-    for level in schema
-        push!(levels, [c  for (i, c) in enumerate(level) if ((i-2) % 4 == 0)])
-    end
-    n = (length(first(schema))+1)÷4
-    stacks = [[] for _ in 1:n]
-    stacks, levels
-end
-
-function build_stacks(schema_string)
-    stacks, levels = parse_schema(schema_string)
-    for crates in levels
-        for (i, crate) in enumerate(crates)
-            !isspace(crate) ? push!(stacks[i], crate) : nothing
+function get_stacks(schema)
+    z = split(schema, "\n")
+    stacks = [[] for _ in 1:length(split(z[end]))]
+    levels = [l[2:4:end] for l in z[1:end-1]]
+    for items in levels
+        for (i, l) in enumerate(items)
+            !isspace(l) && push!(stacks[i], l)
         end
     end
     stacks
 end
 
-parse_moves(moves) = map(x -> parse.(Int64, [x[2], x[4], x[6]]), split.(moves))
+parse_moves(moves) = [filter(x -> contains(x, r"\d+"), z) for z in split.(split(moves, "\n"))]
 
 function rearrange!(stacks, move, cratemover = 9000)
-    n, f, t = move
+    n, f, t = parse.(Int, move)
     tomove = [popfirst!(stacks[f]) for _ in 1:n]
     cratemover == 9001 ? reverse!(tomove) : nothing
     for crate in tomove
@@ -35,10 +21,11 @@ function rearrange!(stacks, move, cratemover = 9000)
     end
 end
 
+
 function run(filename, cratemover)
-    schema_strings, move_strings = split_input(filename)
-    stacks = build_stacks(schema_strings)
-    moves = parse_moves(move_strings)
+    schema, moves_string = split(rstrip(read(open(filename), String)), "\n\n")
+    stacks = get_stacks(schema)
+    moves = parse_moves(moves_string)
     for move in moves
         rearrange!(stacks, move, cratemover)
     end
@@ -48,4 +35,3 @@ end
 filename = "data/5.txt"
 run(filename, 9000)
 run(filename, 9001)
-
